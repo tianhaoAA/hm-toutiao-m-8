4205251f52b4bcf2ec470242a28e8f96428cc6b2
tianhaoAA <1334750953@qq.com>
2020-05-23 22:55:38 +0800

    完成rem适配

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vant from 'vant'
 import 'vant/lib/index.css'
+import 'amfe-flexible'
 Vue.use(vant)
 
 Vue.config.productionTip = false

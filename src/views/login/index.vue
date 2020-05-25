3b82a4a270dd4bbe56740deb7a2cf4cdd0c2364a
tianhaoAA <1334750953@qq.com>
2020-05-25 23:27:45 +0800

    登录页面的布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,16 @@
 <template>
 <div class="container">
      <van-nav-bar title="登录" left-arrow  @click-left="$router.go(-1)"></van-nav-bar>
+  <!-- 登录布局 -->
+  <van-cell-group>
+    <van-field label="手机号" placeholder="请输入手机号"></van-field>
+    <van-field label="验证码" placeholder="请输入验证码">
+      <van-button style="button" size="small" type="primary"></van-button>
+    </van-field>
+  </van-cell-group>
+<div class="login-box">
+    <van-button type="info" round block  size="small">登录</van-button>
+</div>
   </div>
 </template>
 
@@ -11,5 +21,7 @@ export default {
 </script>
 
 <style>
-
+ .login-box {
+   margin: 20px;
+ }
 </style>

43c349fd60b974c1ce0736eff1c72363369b52a9
tianhaoAA <1334750953@qq.com>
2020-05-27 10:10:29 +0800

    article-列表文章布局

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -3,14 +3,26 @@
   <!-- van-list 可以帮助我们 实现上拉加载 需要一些变量 -->
   <!-- 需要做阅读记忆 -->
   <div class="scroll-wrapper">
-
-    <van-pull-refresh @refresh='onRefresh' v-model="downLoading" :success-text="successText">
-          <van-list v-model="upLoading"   finished-text='没有更多数据了' :finished="finished" @load="onLiad">
+    <van-pull-refresh @refresh="onRefresh" v-model="downLoading" :success-text="successText">
+      <van-list v-model="upLoading" finished-text="没有更多数据了" :finished="finished" @load="onLiad">
         <!-- 循环内容 -->
         <van-cell-group>
-  <van-cell title="标题"  :value="'内容'+item" v-for="item in artcles" :key="item"></van-cell>
+          <van-cell v-for="item in artcles" :key="item">
+            <!-- 三张图 -->
+            <div class="article_item">
+              <h3 class="van-ellipsis">2020年你好啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊</h3>
+              <div class="img_box">
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div>
+              <!-- <div class="img_box">
+                <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div> -->
+            </div>
+          </van-cell>
         </van-cell-group>
-    </van-list>
+      </van-list>
     </van-pull-refresh>
   </div>
 </template>
@@ -47,7 +59,10 @@ export default {
       console.log('下拉刷新')
 
       setTimeout(() => {
-        const arr = Array.from(Array(2), (value, index) => '追加' + (index + 1))
+        const arr = Array.from(
+          Array(2),
+          (value, index) => '追加' + (index + 1)
+        )
         this.artcles.unshift(...arr)
         this.downLoading = false
         this.successText = `更新了${arr.length}条数据`
@@ -57,5 +72,45 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span {
+      padding-right: 10px;
+      &.close {
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+  }
+}
 </style>

fd03a204f7f9b71152be1202bab68d191d262afc
tianhaoAA <1334750953@qq.com>
2020-05-28 16:13:04 +0800

    搜索中心-历史记录下

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -3,7 +3,7 @@
     <!-- 搜索组件一级路由   返回上一个页面-->
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
     <!-- 导航 -->
-    <van-search v-model.trim="q"  placeholder="请输入搜索关键词" shape="round" />
+    <van-search @search='onSearch' v-model.trim="q"  placeholder="请输入搜索关键词" shape="round" />
     <van-cell-group class="suggest-box" v-if="q" >
       <van-cell icon="search">
         <span>j</span>ava
@@ -12,13 +12,13 @@
     <div class="history-box" v-if="!q">
       <div class="head"  v-if="historyList.length>0">
         <span>历史记录</span>
-        <van-icon name="delete"></van-icon>
+        <van-icon @click="clear" name="delete"></van-icon>
       </div>
       <van-cell-group>
         <!-- 如果没有历史记录 就不去显示 -->
-        <van-cell v-for="(item,index) in historyList" :key="index">
+        <van-cell @click='toSearchlist(item)' v-for="(item,index) in historyList" :key="index">
           <a class="word_btn">{{item}}</a>
-          <van-icon class="close_btn" @click="delHistory(index)" slot="right-icon" name="cross" />
+          <van-icon class="close_btn" @click.stop="delHistory(index)" slot="right-icon" name="cross" />
         </van-cell>
       </van-cell-group>
     </div>
@@ -41,11 +41,42 @@ export default {
     this.historyList = JSON.parse(localStorage.getItem(key) || '[]')
   },
   methods: {
+    onSearch () {
+      // 判断是否为空 为空直接跳出
+      if (!this.q) return
+      this.historyList.push(this.q)
+      //  这里要去重 set
+      this.historyList = Array.from(new Set(this.historyList))
+      localStorage.setItem(key, JSON.stringify(this.historyList))
+      this.$router.push({
+        path: '/search/result',
+        query: { q: this.q }
+      })
+    },
+    toSearchlist (q) {
+      this.$router.push({
+        path: '/search/result',
+        query: { q }
+      })
+    },
     // 点击删除 删除历史记录
     delHistory (index) {
       // 删除对应的历史记录
       this.historyList.splice(index, 1)
       JSON.parse(localStorage.setItem(key, JSON.stringify(this.historyList)))
+    },
+    async  clear () {
+      // 清空历史记录
+      try {
+        await this.$dialog.confirm({
+          title: '提示',
+          message: '您确定要删除所有的历史记录吗'
+        })
+        this.historyList = []
+        localStorage.setItem(key, '[]')
+      } catch (error) {
+
+      }
     }
   }
 }

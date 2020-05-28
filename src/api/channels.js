401fbe8f91447b5355dc69e1d6227afbc9346001
tianhaoAA <1334750953@qq.com>
2020-05-28 10:22:56 +0800

    重构我的频道API

--- src/api/channels.js
+++ src/api/channels.js
@@ -2,11 +2,38 @@
  *  专门处理频道的请求
  * **/
 import request from '@/utils/request'
-
+import store from '@/store'
+// 游客的 key
+const CACHE_CHANNEL_V = 'heima-8-v'
+// 登录的 key
+const CACHE_CHANNEL_T = 'heima-8-t'
 export function getMyChannels () {
-  return request({
-    url: '/user/channels'
+  // 我们要对原来的方法 进行改造 但是不想
+  return new Promise(function (resolve, reject) {
+    // 支持本地化的缓存  要区分当前是登录用户 还是游客用户
+    // 可以根据 当前有没有token区分有没有登录
+    const key = store.state.user.token ? CACHE_CHANNEL_V : CACHE_CHANNEL_T
+    // 去缓存中读取 用户频道数据
+    const str = localStorage.getItem(key)
+    if (str) {
+    //  本地缓存有数据
+      resolve({ channels: JSON.parse(str) })
+    } else {
+      // 本地缓存没有数据 要去线上拉取数据 得到线上的用户信息
+
+      request({
+        url: '/user/channels'
+      }).then(res => {
+      //  获取请求的结果
+        localStorage.setItem(key, JSON.stringify(res.channels))
+        // 这里表示 直接 成功执行 获取用户的数据
+        resolve(res)
+      })
+    }
   })
+  // return request({
+  //   url: '/user/channels'
+  // })
 }
 
 /***

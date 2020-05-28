2d4d955197a58ffe2c90ba8f30264b04e4871e1b
tianhaoAA <1334750953@qq.com>
2020-05-28 11:17:00 +0800

    添加我的频道api

--- src/api/channels.js
+++ src/api/channels.js
@@ -68,3 +68,21 @@ export function delChannels (id) {
     }
   })
 }
+
+/****
+ *
+ *   添加频道的方法
+ *     @param 传入频道对象
+ * **/
+export function addChannels (channels) {
+  return new Promise(function (resolve, reject) {
+    const key = store.state.user.token ? CACHE_CHANNEL_V : CACHE_CHANNEL_T
+    const channels = JSON.parse(localStorage.getItem(key))
+    // 将添加的频道数据添加到队尾
+    channels.push(channels)
+    // 重新写入缓存
+    localStorage.setItem(key, JSON.stringify(channels))
+    // 执行这一步 相当于 告诉我们使用promise的方法 执行成功
+    resolve()
+  })
+}

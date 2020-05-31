2b6bfdb8d0b0392ba50d867c42c84fe1283b403b
tianhaoAA <1334750953@qq.com>
2020-05-31 15:35:03 +0800

    项目打包

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,12 @@ module.exports = {
         }
       }
     }
-  }
+  },
+  configureWebpack: (config) => {
+    // config 参数 就是当前 vue-cli 项目所以的webpack 设置
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    }
+  },
+  publicPath: './'
 }
